99a43ceab1b8ef3aa38a43ff283f953019437fd4
Fred Baa <frederickbaa@gmail.com>
2019-05-18 14:54:50 +0800

    added form fields and initial computation for splitting equally

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,136 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(BillSplitterMain());
+}
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class BillSplitterMain extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      title: 'BillSplitter',
+      home: BillSplitter(),
+      theme: ThemeData( 
+        primaryColor: Colors.red,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class BillSplitterState extends State<BillSplitter> {
+  double subTotalAmount = 0.0;
+  double serviceChargeAmount = 0.0;
+  double taxAmount = 0.0;
+  int personCount = 1;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    TextField billAmountField = new TextField(
+        decoration: new InputDecoration(labelText: "Subtotal", labelStyle: TextStyle(fontSize: 20)),
+        keyboardType: TextInputType.number,
+        style: TextStyle(fontSize: 20),
+        onChanged: (String value) {
+          try {
+            subTotalAmount = double.parse(value);
+          } catch (exception) {
+            subTotalAmount = 0.0;
+          }
+        },
     );
+
+    TextField serviceChargeField = new TextField(
+        decoration: new InputDecoration(labelText: "Service Charge", labelStyle: TextStyle(fontSize: 20)),
+        keyboardType: TextInputType.number,
+        style: TextStyle(fontSize: 20),
+        onChanged: (String value) {
+          try {
+            serviceChargeAmount = double.parse(value);
+          } catch (exception) {
+            serviceChargeAmount = 0.0;
+          }
+        });
+
+    TextField taxAmountField = new TextField(
+        decoration: new InputDecoration(labelText: "Tax Amount", labelStyle: TextStyle(fontSize: 20)),
+        keyboardType: TextInputType.number,
+        style: TextStyle(fontSize: 20),
+        onChanged: (String value) {
+          try {
+            taxAmount = double.parse(value);
+          } catch (exception) {
+            taxAmount = 0.0;
+          }
+        });
+
+    TextField personCountField = new TextField(
+        decoration: new InputDecoration(labelText: "No. of people", labelStyle: TextStyle(fontSize: 20)),
+        keyboardType: TextInputType.number,
+        style: TextStyle(fontSize: 20),
+        onChanged: (String value) {
+          try {
+            personCount = int.parse(value);
+          } catch (exception) {
+            personCount = 1;
+          }
+        });
+
+    MaterialButton calculateEquallyButton = new MaterialButton(
+        child: new Text("Split Equally"),
+        color: Colors.red,
+        textColor: Colors.white,
+        onPressed: () {
+          double total = subTotalAmount + serviceChargeAmount + taxAmount;
+          total = total / personCount;
+
+          String formattedTotal = total.toStringAsFixed(2);
+
+          AlertDialog dialog = new AlertDialog(
+              content: new Text("Total is $formattedTotal per person"));
+
+          showDialog(context: context, builder: (BuildContext context) => dialog);
+        });
+
+    MaterialButton splitByPersonButton = new MaterialButton(
+        child: new Text("Split By Person"),
+        color: Colors.red,
+        textColor: Colors.white,
+        onPressed: () {
+          AlertDialog dialog = new AlertDialog(
+                content: new Text("TODO"));
+          showDialog(context: context, builder: (BuildContext context) => dialog);
+        });
+
+    Container container = new Container(
+        padding: const EdgeInsets.all(16.0),
+        child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              billAmountField,
+              serviceChargeField,
+              taxAmountField,
+              personCountField,
+              Container(
+                alignment: Alignment(0.0, 0.0),
+                child: Row(children: <Widget>[
+                  new Expanded(child: Container(),),
+                  calculateEquallyButton,
+                  Padding(
+                    padding: EdgeInsets.only(left: 5.0),
+                  ), 
+                  splitByPersonButton,
+                  new Expanded(child: Container(),),
+                ],),
+              ),
+            ]));
+
+    AppBar appBar = new AppBar(title: new Text("BillSplitter"), backgroundColor: Colors.red,);
+
+    Scaffold scaffold = new Scaffold(appBar: appBar, body: container);
+    return scaffold;
   }
+}
+
+class BillSplitter extends StatefulWidget {
+  @override
+  BillSplitterState createState() => BillSplitterState();
 }
